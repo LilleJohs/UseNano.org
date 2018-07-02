847af28bbf5d6435b387002a583b05b7dc465ffe
LilleJohs <33736881+LilleJohs@users.noreply.github.com>
2018-07-02 17:33:26 +0200

    bug fix

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:5000/db?search=';
+const baseURL = location.protocol + '//' + location.host + '/db?search=';
 
 export const doSearch = (searchString) => async dispatch => {
   const url = baseURL + searchString + "&page=1";
