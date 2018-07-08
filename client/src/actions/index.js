17863f04f4f6567a3a21010e53416edcfc38c925
LilleJohs <33736881+LilleJohs@users.noreply.github.com>
2018-07-08 19:41:10 +0200

    usenano.org

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://paywithrai.com/db?search=';
+const baseURL = 'https://www.usenano.org/db?search=';
 
 export const doSearch = (searchString) => async dispatch => {
   const url = baseURL + searchString;
