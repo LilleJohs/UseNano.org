32e12b426f1a0c62a446456ba5e199974b6c3ab2
LilleJohs <33736881+LilleJohs@users.noreply.github.com>
2018-07-08 19:59:05 +0200

    bug fix

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ const helmet = require('helmet');
 
 const app = express();
 
-function wwwRedirect(req, res, next) {
-    if (req.headers.host.slice(0, 4) === 'www.') {
-        var newHost = req.headers.host.slice(4);
-        return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
-    }
-    next();
-};
-
 app.set('trust proxy', true);
 app.use(wwwRedirect);
 
