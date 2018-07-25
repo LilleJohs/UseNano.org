e6f162d4ec21dc2cb0b409d690bd345a23c886cf
Georg Rieger <georg.rieger@eblcom.ch>
2018-07-25 19:30:17 +0200

    Change maps from Google Maps to OSM/Mapbox and add locating feature

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import React, { Component } from 'react'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import axios from 'axios';
 
-export class MapContainer extends Component {
+
+export default class MapContainer extends Component {
   constructor(props) {
     super(props);
 
@@ -13,7 +14,20 @@ export class MapContainer extends Component {
       selectedPlace: {}
     };
   }
+  
+  
+  updateDimensions() {
+    const height = window.innerHeight - 65;
+    this.setState({ height: height })
+  }
 
+  componentWillMount() {
+    this.updateDimensions()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions.bind(this))
+  }
   componentDidMount() {
     let baseURL;
     if (process.env.NODE_ENV === 'production') {
@@ -21,32 +35,29 @@ export class MapContainer extends Component {
     } else {
       baseURL = 'http://localhost:5000/mapdb';
     }
+    
+    window.addEventListener("resize", this.updateDimensions.bind(this));
+    
+    const leafletMap = this.leafletMap.leafletElement;
+    
+    leafletMap.locate({setView: true, maxZoom: 7});
 
     axios.get(baseURL).then(res => {
       this.setState({ stores: res.data });
     });
   }
 
-  onMarkerClick = (props, marker, e) =>
-     this.setState({
-       selectedPlace: props,
-       activeMarker: marker,
-       showingInfoWindow: true
-     });
-
   renderOne(store, i) {
     return (
-      <Marker
-        onClick={this.onMarkerClick}
-        title={store.name}
-        name={store.category}
-        website={store.website}
-        key={i}
-        position={{ lat: store.lat, lng: store.lng }}
-      />
+      <Marker key={i} position={{lat: store.lat, lng: store.lng}}>
+		<Popup style={{margin: '0'}}>
+				<h3>{store.name}</h3>
+				<h4>{store.category}</h4>
+				<h4><a href={store.website}>{store.website}</a></h4></Popup>
+      </Marker>
     );
   }
-
+  
   renderAll() {
     let table = [];
     let { stores } = this.state;
@@ -56,35 +67,18 @@ export class MapContainer extends Component {
     return table;
   }
 
+  
   render() {
+    const position = {lat: 0, lng: 0};
     return (
-      <div style={{ margin: '-24px 0px -90px 0px' }}>
-        <Map
-          google={this.props.google}
-          initialCenter={{
-            lat: 50,
-            lng: 10
-          }}
-          zoom={5}
-        >
-          {this.renderAll()}
-
-          <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}
-          >
-            <div style={{margin: '-10px 0px 0px 0px'}}>
-              <h3>{this.state.selectedPlace.title}</h3>
-              <h4>{this.state.selectedPlace.name}</h4>
-              <h4><a target="_blank" href={this.state.selectedPlace.website}>{this.state.selectedPlace.website}</a></h4>
-            </div>
-          </InfoWindow>
-        </Map>
-      </div>
-    );
+      <Map ref={m => { this.leafletMap = m; }} center={position} zoom={2}  style={{ height: this.state.height}}>
+        <TileLayer
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+          url="https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiamlpa3V5IiwiYSI6ImNqazE3amtlYzBhbWQzd2xleHNicWp3bXgifQ.yzR_F6OSDRflAc52jhi3Nw"
+          maxZoom="18"
+        />
+        {this.renderAll()}
+      </Map>
+    )
   }
 }
-
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyD3INZ5BSFvJLSl-eQONSY7BvvTNJGyTFo'
-})(MapContainer);
